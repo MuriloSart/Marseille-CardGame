24499808b3486201b898f750bf54166b5fa0b6c9
Murilo Sartori <89427588+MuriloSart@users.noreply.github.com>
2024-04-27 15:21:22 -0300

    Add  Color Type

--- Assets/Scripts/Cards/CardPack.cs
+++ Assets/Scripts/Cards/CardPack.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardPack : MonoBehaviour
 {
     [SerializeField] private GameObject card;
     public List<GameObject> cards;
+
+    //Privates
+    private Color _paintColor = Color.green; 
+
     private void Awake()
     {
         int decimalCount = 0;
@@ -15,6 +20,14 @@ public class CardPack : MonoBehaviour
             var obj = Instantiate(card);
             obj.transform.SetParent(this.transform);
             obj.transform.position = this.transform.position;
+            obj.GetComponent<Image>().color = _paintColor;
+
+            if(i > 10 && i <= 20)
+                _paintColor = Color.red;
+            else if(i > 20 && i <= 30)
+                _paintColor = Color.black;
+            else if(i > 30 && i <= 40)
+                _paintColor = Color.yellow;
 
             int index = decimalCount / 10;
             if (index >= 4)
